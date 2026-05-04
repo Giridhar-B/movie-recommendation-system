94e140a40afa5f29d50eab995480b50802d4e30c
Giridhar-B <90565386+Giridhar-B@users.noreply.github.com>
2026-05-04 20:15:15 +0530

    Update About.jsx

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -102,7 +102,7 @@ export default function About() {
         </h2>
 
         <a
-          href="https://github.com/your-username/movie-recommender"
+          href="github.com/Giridhar-B/movie-recommendation-system"
           target="_blank"
           rel="noopener noreferrer"
           className="text-blue-500 underline"
